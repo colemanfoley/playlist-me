169ce4ae989162dc14c679a6b1f936ebbcaf5653
Coleman Foley <colemanfoley@gmail.com>
2013-03-03 19:39:26 -0800

    Added comments, mucked around with setting up a listener for the answer button.

--- playlist-me.js
+++ playlist-me.js
@@ -1,24 +1,38 @@
 if (Meteor.isClient) {
+  //Initial set-up: specifying which fields accounts-ui should have
+  //and making the questions collection.
   Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
   });
 
   Questions = new Meteor.Collection("questions");
-
+  //A helper that returns a greeting to the intro template.
   Template.hello.greet = function () {
     return "Enter a question to get music recommendations.";
   };
 
+  //Handlers for the hello template.
   Template.hello.events({
-    'click .btn' : function () {
+    'click .ask' : function () {
       // template data, if any, is available in 'this'
       var question = {};
       question.text = $('#questionInput').val();
       question.user = Meteor.user();
+      question.answers = [];
       Questions.insert(question);
     }
   });
 
+  //Handlers for the showQuestions template.
+  Template.showQuestions.events({
+    //This handler should be on each question, not on the whole question template?
+    // 'click .answer' : function () {
+    //   var answer = {};
+    //   answer.user = Meteor.user();
+    //   answer.text = $('.answerInput');
+    //   this.
+    // }
+  });
   Template.showQuestions.questions = function () {
     return Questions.find({});
   };
