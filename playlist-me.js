fef59a9eb3943c835a018516c31359f7a72fc4cf
Coleman Foley <colemanfoley@gmail.com>
2013-03-03 17:31:02 -0800

    Added accounts packages and Bootstrap package. Renamed hello template.

--- playlist-me.js
+++ playlist-me.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to playlist-me.";
+  Template.hello.askQuestion = function () {
+    return "Enter a question to get music recommendations.";
   };
 
   Template.hello.events({
