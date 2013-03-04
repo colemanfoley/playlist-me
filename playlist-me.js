2951ee4248c9db5d467e64e843bf04bf237bdd15
Coleman Foley <colemanfoley@gmail.com>
2013-03-03 18:44:12 -0800

    Displays all questions, complete with username.

--- playlist-me.js
+++ playlist-me.js
@@ -1,19 +1,35 @@
 if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+  });
+
+  Questions = new Meteor.Collection("questions");
+
   Template.hello.greet = function () {
     return "Enter a question to get music recommendations.";
   };
 
   Template.hello.events({
-    'click input' : function () {
+    'click .btn' : function () {
       // template data, if any, is available in 'this'
-      var questionText = $('#questionInput').val();
-      console.log(questionText);
+      var question = {};
+      question.text = $('#questionInput').val();
+      question.user = Meteor.user();
+      console.log(question);
+      console.log(Questions);
+      Questions.insert(question);
+      console.log(Questions.find());
     }
   });
+
+  Template.showQuestions.questions = function () {
+    return Questions.find({});
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    Questions = new Meteor.Collection("questions");
     // code to run on server at startup
   });
 }
