1bc616a048c3eead8e667711f570b4a3ac4fe041
Hack Reactor Students <accounts@hackreactor.com>
2013-03-10 10:55:53 -0700

    Play button in answer template now plays a guess at what the song is. Can neither resume song after pause nor play one song, then another.

--- client/answerTemplate.js
+++ client/answerTemplate.js
@@ -2,9 +2,10 @@ var songToPlay = "";
 Template.answer.events({
   'click .play' : function () {
     $('#placeholder').rdio("GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=");
+    var songToSend = this.text;
     $('#placeholder').bind('ready.rdio', function(e) {
-      Meteor.http.post("http://localhost:8080", {data: {songKey: "hi"}},function(error, result){
-        songToPlay = result.content;
+      Meteor.http.post("http://localhost:8080", {content: songToSend},function(error, result){
+        songToPlay = JSON.parse(result.content).key;
         $('#placeholder').rdio().play(songToPlay);
       });
     });
