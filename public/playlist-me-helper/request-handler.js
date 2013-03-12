b875fa157e4ecd89b6100d4a001d2d8e58763dbd
Hack Reactor Students <accounts@hackreactor.com>
2013-03-12 16:25:41 -0700

    Began setting up if condition in request handler, still works.

--- public/playlist-me-helper/request-handler.js
+++ public/playlist-me-helper/request-handler.js
@@ -3,17 +3,25 @@ var globals = require('./globals.js');
 exports.requestHandler = function (request, response) {
   console.log("Serving request type " + request.method + " for url " + request.url);
   var statusCode = 200;
-  var fullQuery = "";
-  request.on('data', function(chunk) {
-    fullQuery += chunk.toString();
-  });
   var headers = globals.defaultCorsHeaders;
   headers['Content-Type'] = "text/plain";
-  response.writeHead(statusCode, headers);
 
-  request.on('end', function (argument) {
-    globals.r.makeRequest('search', {query: fullQuery, types: 'Track'}, function() {
-      response.end(JSON.stringify(arguments[1].result.results[0]));
+  var fullQuery = "";
+  if (request.method === "OPTIONS") {
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+  else {
+    request.on('data', function(chunk) {
+      fullQuery += chunk.toString();
     });
-  });
+
+    response.writeHead(statusCode, headers);
+
+    request.on('end', function (argument) {
+      globals.r.makeRequest('search', {query: fullQuery, types: 'Track'}, function() {
+        response.end(JSON.stringify(arguments[1].result.results[0]));
+      });
+    });
+  };
 };
