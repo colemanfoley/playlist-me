e96ae7fa78871bee9c49e53218061bc8f347c506
Coleman Foley <colemanfoley@gmail.com>
2013-03-15 09:40:21 -0700

    Now playing an answer consistently plays the right song.

--- client/searchResultTemplate.js
+++ client/searchResultTemplate.js
@@ -1,7 +1,8 @@
 Template.searchResult.events({
   'click .answer' : function (e) {
+    console.log(this.key);
     Questions.update({_id: Session.get("questionToShow")},
-      {$push: {answers: {text: this.name, user:Meteor.user(), createdAt: Date.now()} } }
+      {$push: {answers: {text: this.name, key: this.key, user:Meteor.user(), createdAt: Date.now()} } }
     );
   }
 });
