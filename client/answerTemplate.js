801b5a36c08bdb691d9f65159e48c276492c3ece
Coleman Foley <colemanfoley@gmail.com>
2013-03-14 23:14:46 -0700

    Cleaned up answer and answers list templates, removing obsolete search functionality.

--- client/answerTemplate.js
+++ client/answerTemplate.js
@@ -21,39 +21,5 @@ Template.answer.events({
 
   'click .pause' : function () {
     $('#placeholder').rdio().pause();
-  },
-
-  'click .search' : function (e) {
-    Session.set("currentAnswer", this.createdAt);
-    var queryToSend = this.text;
-    var searchResults = "";
-    SearchResultsCollection.remove({query: queryToSend})
-    Meteor.http.post("http://localhost:8080", {data: {key: queryToSend, queryType: "search"}},
-      function(error, result){
-      if(error){
-        console.log(error);
-      }
-      searchResults = JSON.parse(result.content);
-      var d = {
-        query: queryToSend,
-        result: searchResults
-      }
-
-      SearchResultsCollection.insert(d);
-
-      Session.set("searchQuery", queryToSend);
-    });
   }
 });
-
-Template.answer.searchResults = function(){
-  var currentSearchQuery = Session.get("searchQuery");
-  var result = SearchResultsCollection.find({
-    query: currentSearchQuery
-  });
-  return result;
-};
-
-Template.answer.currentAnswer = function () {
-  return Session.equals("currentAnswer", this.createdAt);
-};
