52886d22d6aa892cc583bb913fb66e06e2bbb2e3
Coleman Foley <colemanfoley@gmail.com>
2013-03-15 09:49:36 -0700

    Each answer now shows both artist and title.

--- client/searchResultTemplate.js
+++ client/searchResultTemplate.js
@@ -2,7 +2,7 @@ Template.searchResult.events({
   'click .answer' : function (e) {
     console.log(this.key);
     Questions.update({_id: Session.get("questionToShow")},
-      {$push: {answers: {text: this.name, key: this.key, user:Meteor.user(), createdAt: Date.now()} } }
+      {$push: {answers: {title: this.name, artist: this.artist, key: this.key, user:Meteor.user(), createdAt: Date.now()} } }
     );
   }
 });
