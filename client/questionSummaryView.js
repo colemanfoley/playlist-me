cb26ab6602f3a42c7f99950ac5a34e1d0fcdfecf
Coleman Foley <colemanfoley@gmail.com>
2013-04-07 21:00:04 -0700

    Reorganized client-side files. In individual question view, question title no longer appears as a link.

--- client/questionSummaryView.js
+++
@@ -1,6 +0,0 @@
-//If a question title is clicked, that question is to be displayed in the detailed view.
-Template.questionSummary.events({
-	'click .questionTitle' : function (){
-		Session.set("questionToShow", this._id);
-	}
-});
--- client/views/questionList.js
+++ client/views/questionList.js
@@ -26,3 +26,10 @@ Template.questionList.questionToShow = function () {
 Template.questionList.showOneQuestion = function () {
 	return Questions.find({_id: Session.get("questionToShow")});
 };
+
+//If a question title is clicked, that question is to be displayed in the detailed view.
+Template.questionSummary.events({
+	'click .questionTitle' : function (){
+		Session.set("questionToShow", this._id);
+	}
+});
